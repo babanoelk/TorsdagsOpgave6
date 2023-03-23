1e8c5a3945ebbba441a0337d0543a17f9f7732fb
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-03-23 11:31:40 +0100

    Done.

--- TorsdagsOpgave6/src/TaskFour/Circle.java
+++ TorsdagsOpgave6/src/TaskFour/Circle.java
@@ -1,5 +1,9 @@
 package TaskFour;
 
+import com.sun.corba.se.impl.interceptors.PICurrent;
+
+import static java.lang.Math.PI;
+
 public class Circle implements Shape {
 
     double radius;
@@ -10,7 +14,7 @@ public class Circle implements Shape {
 
     @Override
     public double getArea() {
-        return 3.14 * (radius * radius);
+        return PI * (radius * radius);
     }
 
     /* 4.2 Lav to klasser, Circle og Square, som implementerer interfacet. Lad de to klasser tage en double som
